784e55fe0276175567714b5106c9daedd7d36cb4
Spencer Burke <info.spencer.burke@gmail.com>
2021-04-15 23:26:52 -0700

    Adding some basic code from a tutorial to get a basic parse working.

--- main.go
+++ main.go
@@ -9,5 +9,11 @@ import (
 
 func main() {
 //currently nothing yet
+    text_ptr := flag.String("text", "", "Text to parse.")
+    metric_ptr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
+    unique_ptr := flag.Bool("unique", false, "Measure unique values of a metric.")
+    flag.Parse()
+
+    fmt.Printf("text_ptr: %s, metric_ptr: %s, unique_ptr: %t\n", *text_ptr, *metric_ptr, *unique_ptr)
 }
 
